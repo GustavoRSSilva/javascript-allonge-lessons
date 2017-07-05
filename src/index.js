0ed1da77475c74005b70e234cec0e58e4c52a31f
Gustavo Silva <gustavo.silva@forkit.pt>
2017-07-05 12:49:42 +0100

    [new] diferente types of functions give different outputs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 (
-  () => console.log("Welcome")
-
+  () => console.log('Welcome')
 )();
+
+// Why you should avoid using floating point equations with javascript
+console.log('why you should avoid using floating point equations on Js');
+console.log(`the some of 1.0 + 1.0 + 1.0 equals to ${1.0 + 1.0 + 1.0}`);
+console.log('while the some of decimals numbers is:');
+console.log(`0.1 + 0.1 + 0.1 = ${0.1 + 0.1 + 0.1}`);
+
+
+// TODO
+const func1 = (function() {
+  return (function() { return arguments[0]; })('inner');
+})('outer');
+console.log(func1);
+
+const func2 = (function() {
+  return (() => arguments[0])('inner');
+})('outer');
+console.log(func2);
