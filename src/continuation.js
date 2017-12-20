0b52292597dd4353a04778603de755993e554da1
GustavoRSSilva <gustavorssilva09@gmail.com>
2017-12-20 21:31:40 +0000

    [new] up to 206

--- src/continuation.js
+++ src/continuation.js
@@ -79,3 +79,49 @@ console.log('another way');
 // }
 
 console.log('let a, b, state = 0;\n\nconst fibonacci = () => {\n  switch (state) {\n    case 0:\n      state = 1;\n      return a = 0;\n    case 1:\n      state = 2;\n      return b = 1;\n    case 2:\n      [a, b] = [b, a + b];\n      return b;\n  }\n}');
+
+//  pag207
+console.log('Javascrit generators');
+console.log('An iterator written in a generator style is called generator.');
+console.log('We declate the function using the function * syntax');
+console.log('We do not return values or outputs them to the console. We "yeild" the value using the yield keyword.');
+
+function * empty() {};
+
+console.log(empty().next());
+
+function * only(something) {
+  yield something;
+}
+
+
+const yooo = only("yoo");
+
+console.log('const yooo = only("yoo");');
+console.log(yooo.next());
+console.log(yooo.next());
+
+
+const oneTwoThree = function * () {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+console.log('const oneTwoThree = function * () {\n  yield 1;\n  yield 2;\n  yield 3;\n}');
+console.log(oneTwoThree().next());
+console.log(oneTwoThree().next());
+console.log(oneTwoThree().next());
+console.log(oneTwoThree().next());
+
+const iterator123 = oneTwoThree();
+console.log('const iterator123 = oneTwoThree();');
+console.log(iterator123.next());
+console.log(iterator123.next());
+console.log(iterator123.next());
+console.log(iterator123.next());
+
+console.log('Generators behave very differently form normal functions, what happens semantically');
+console.log('1- We call oneTwoThree() and get an iterator');
+console.log('2- The iterator is in a nascent or in a "newBorn" state.');
+console.log('3- When we call interator.next(), the bpdy of our generator  begins to be evaluated.');
