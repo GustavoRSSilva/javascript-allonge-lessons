e2eb74dc4de1ddc831f1b013afcee3e665e0546b
Gustavo Silva <gustavosilva@Gustavos-MBP.home>
2017-12-23 22:55:37 +0000

    [new] up to page 230

--- src/continuation.js
+++ src/continuation.js
@@ -121,7 +121,93 @@ console.log(iterator123.next());
 console.log(iterator123.next());
 
 console.log('Generators behave very differently form normal functions, what happens semantically');
-console.log('1- We call oneTwoThree() and get an iterator');
-console.log('2- The iterator is in a nascent or in a "newBorn" state.');
-console.log('3- When we call interator.next(), the bpdy of our generator  begins to be evaluated.');
+console.log('1. We call oneTwoThree() and get an iterator');
+console.log('2. The iterator is in a nascent or in a "newBorn" state.');
+console.log('3. When we call interator.next(), the bpdy of our generator  begins to be evaluated.');
+console.log('4.The body of our generators runs until it returns, ends, or encounters a yield statement, which is yield 1.');
+console.log('- The iterator suspends its execution.');
+console.log('- the iterator wraps 1 in {done: false, value: 1} and returns that from the call to the .next().');
+console.log('- The rest of the program continues along its way until it ,+makes another call to iterator .next().');
+console.log('- The iterator resumes execution from the point where it yieled last value.');
+console.log('5.The body of our generators runs until it returns, ends, or encounters a yield statement, which is yield 2.');
+console.log('- The iterator suspends its execution.');
+console.log('- the iterator wraps 2 in {done: false, value: 2} and returns that from the call to the .next().');
+console.log('- The rest of the program continues along its way until it ,+makes another call to iterator .next().');
+console.log('- The iterator resumes execution from the point where it yieled last value.');
+console.log('6.The body of our generators runs until it returns, ends, or encounters a yield statement, which is yield 2.');
+console.log('- The iterator suspends its execution.');
+console.log('- the iterator wraps 3 in {done: false, value: 3} and returns that from the call to the .next().');
+console.log('- The rest of the program continues along its way until it ,+makes another call to iterator .next().');
+console.log('- The iterator resumes execution from the point where it yieled last value.');
+console.log('6.The body of our generators runs until it returns, ends, or encounters a yield statement, there are no more lines of code, so it ends');
+console.log('- the iterator return {done: true} from the call to .next(), and every call to this iterator\'s .next() methods will return {done: true} from now on.');
+console.log('This behaviour is not unique to javascript, generators are called coroutines in other languages.');
+console.log('Coroutine are computer program components the generalize subroutines for nonpreemptive multitasking, by allowin multiples entry points for suspending and resuming execution at certaint locations. Coroutines are well-suited for implementing more familiar program components such as cooperative tasks, exeptions, event loop, iterators, inifite lists and pipes.');
 
+const threeNumbers = {
+  [Symbol.iterator]: function * () {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+};
+
+console.log('const threeNumbers = {\n  [Symbol.iterator]: function * () {\n    yield 1;\n    yield 2;\n    yield 3;\n  }\n};');
+console.log('[...threeNumbers]');
+console.log([...threeNumbers]);
+
+
+// const fibonacci = {
+//   [Symbol.iterator]: () => {
+//     let a = 0, b = 1, state = 0;
+
+//     return {
+//       next: () => {
+//         switch (state) {
+//           case 0:
+//             state = 1;
+//             return {value: a};
+//           case 1:
+//             state = 2;
+//             return {value: b};
+//           case 2:
+//             [a, b] = [b, a + b];
+//             return {value: b};
+//         }
+//       }
+//     }
+//   }
+// }
+
+// for (let n of fibonacci) {
+//   console.log(n);
+// }
+
+console.log('const fibonacci = {\n  [Symbol.iterator]: () => {\n    let a = 0, b = 1, state = 0;\n    return {\n      next: () => {\n        switch (state) {\n          case 0:\n            state = 1;\n            return {value: a};\n          case 1:\n            state = 2;\n            return {value: b};\n          case 2:\n            [a, b] = [b, a + b];\n            return {value: b};\n        }\n      }\n    }\n  }\n}\nfor (let n of fibonacci) {\n  console.log(n);\n');
+
+
+// const mapWith = (fn, iterable) => {
+//   ({
+//     [Symbol.iterator]: () => {
+//       const iterator = iterable[Symbol.iterator]();
+//       return {
+//         next: () => {
+//           const {done, value} = iterator.next();
+//           return ({ done: done, value: done ? undefined : fn(value)});
+//         }
+//       }
+//     }
+//   })
+// }
+
+console.log('const mapWith = (fn, iterable) => {\n  ({\n    [Symbol.iterator]: () => {\n      const iterator = iterable[Symbol.iterator]();\n      return {\n        next: () => {\n          const {done, value} = iterator.next();\n          return ({ done: done, value: done ? undefined : fn(value)});\n        }\n      }\n    }\n  })\n}');
+
+console.log('Or you can do this way');
+
+// function * mapWith (fn, iterable) {
+//   for (const element of iterable) {
+//     yield fn(element);
+//   }
+// }
+
+console.log('function * mapWith (fn, iterable) {\n  for (const element of iterable) {\n    yield fn(element);\n  }\n}');
